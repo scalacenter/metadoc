9943626774cd350ba431c12a5eea014de16d1fc2
Jonas Fonseca <jonas.fonseca@gmail.com>
2017-07-06 21:35:49 -0400

    Fix CSS to better handle layout changes

--- metadoc-js/src/main/resources/index.js
+++ metadoc-js/src/main/resources/index.js
@@ -13,7 +13,6 @@ require('node_modules/material-design-icons/iconfont/material-icons.css')
 require('node_modules/material-components-web/dist/material-components-web.css')
 var mdc = require('node_modules/material-components-web/dist/material-components-web.js')
 
-mdc.autoInit()
 var drawerEl = document.querySelector('.mdc-temporary-drawer');
 var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
 var drawer = new MDCTemporaryDrawer(drawerEl);
